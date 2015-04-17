03ec0432887e1680011872bfae7cbc9dbf991967
duftler <duftler@google.com>
2015-04-17 08:15:03 -0400

    Improve missing value error message for all Google property types.

--- src/main/java/com/cloudera/director/google/compute/GoogleComputeInstanceTemplateConfigurationProperty.java
+++ src/main/java/com/cloudera/director/google/compute/GoogleComputeInstanceTemplateConfigurationProperty.java
@@ -61,7 +61,7 @@ public enum GoogleComputeInstanceTemplateConfigurationProperty implements Config
 
   @Override
   public String getMissingValueErrorMessage() {
-    return null;
+    return "'" + configKey + "' is a required property.";
   }
 
   @Override
